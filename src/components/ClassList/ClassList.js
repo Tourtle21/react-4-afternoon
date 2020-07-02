56fb96908ad78aed90dbee7cb52013962fda699a
Tourtle21 <kardlargin@gmail.com>
2020-07-02 13:16:45 -0800

    Finished Project

--- src/components/ClassList/ClassList.js
+++ src/components/ClassList/ClassList.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+import axios from 'axios';
 
 export default class ClassList extends Component {
   constructor() {
     super()
-    
+    this.state = {
+      students: [],
+    }
+  }
+
+  componentDidMount() {
+    axios.get(`http://localhost:3005/students?class=${this.props.match.params.class}`)
+    .then(res => {console.log(res.data); this.setState({students:res.data})})
+    .catch(err => console.log(err));
   }
 
   render() {
+  const mappedStudents = this.state.students.map((student, i) => (<Link key={i} to={`/student/${student.id}`}><h3>{student.first_name} {student.last_name}</h3></Link>))
     return (
       <div className="box">
         <h1></h1>
         <h2>ClassList:</h2>
-
+        {mappedStudents}
+        <button onClick={() => this.props.history.goBack()} className='back-button'>{'<'} Back</button>
       </div>
     )
   }
--- src/components/Student/Student.js
+++ src/components/Student/Student.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+import axios from 'axios';
 
 export default class Student extends Component {
   constructor() {
     super()
+    this.state = {
+      studentInfo: {}
+    }
+  }
 
+  componentDidMount() {
+    axios.get(`http://localhost:3005/students/${this.props.match.params.id}`)
+    .then(res => this.setState({studentInfo: res.data}))
+    .catch(err => console.log(err))
   }
 
   render() {
+    const {first_name, last_name, grade, email} = this.state.studentInfo
     return (
       <div className="box">
         <h1>Student</h1>
+        <h1>{first_name} {last_name}</h1>
+        <h3>Grade: {grade}</h3>
+        <h3>Email: {email}</h3>
+        <button onClick={() => this.props.history.goBack()} className='back-button'>{'<'} Back</button>
       </div>
     )
   }
